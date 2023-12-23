b871204d50da319ad8f24775e759f3c3d70f611a
me-pikachu <rehanc5100@gmail.com>
2023-12-23 20:21:43 +0530

    Uploading files

---
+++ Source/Crossfire/CrossfireGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrossfireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CROSSFIRE_API ACrossfireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
